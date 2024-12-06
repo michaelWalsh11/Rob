9f516a6752787d7960bfa4e4bb6d9a37b0de6b48
michaelWalsh11 <michaelwalsh27@marist.com>
2024-12-05 22:06:34 -0500

    que

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -8,8 +8,8 @@ public class DriveConstants {
     public static double KICKRIGHT_UP = 0.0; //DO
     public static double KICKLEFT_UP = 0.0; //DO
 
-    public static double OUTTAKE_DROP = 0.0; //DO
-    public static double OUTTAKE_INTAKE = 0.0; //DO
+    public static double OUTTAKE_DROP = 0.5;
+    public static double OUTTAKE_INTAKE = 0.85;
 
     public static double GRASPER_OPEN = 0.3;
     public static double GRASPER_CLOSE = 0.85;
@@ -20,6 +20,7 @@ public class DriveConstants {
     public static int INTAKE_MIN = 0;
     public static int INTAKE_MAX = 100; //DO
 
-    public static int OUTTAKE_MAX = 100; //DO
     public static int OUTTAKE_MIN = 0;
+    public static int OUTTAKE_MAX = 100; //DO
+
 }
